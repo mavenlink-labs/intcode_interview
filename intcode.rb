4d3e59fd1ccc058a97950bbfdde1555faa882e56
Chris Bacon <cbacon@mavenlink.com>
2020-08-04 19:59:29 -0600

    the easiest test of all time - return if opcode 99

--- intcode.rb
+++ intcode.rb
@@ -1,3 +1,14 @@
 class Intcode
+  def read_tape(input)
+    tape = input.dup
 
+    position = 0
+    while true
+      code = tape[position]
+      return tape if code == 99
+
+      return nil
+    end
+
+  end
 end
--- intcode_spec.rb
+++ intcode_spec.rb
@@ -2,8 +2,15 @@ require_relative 'spec_helper'
 require_relative 'intcode'
 
 describe Intcode do
-  it 'can be initialized' do
+  it 'returns immediately if the first code is 99' do
+    tape = [99]
     subject = Intcode.new
-    expect(subject).to_not be_nil
+    expect(subject.read_tape(tape)).to eq tape
+  end
+
+  xit 'can add numbers' do
+    tape = [1, 0, 0, 0, 99]
+    subject = Intcode.new
+    expect(subject.read_tape(tape)).to eq [2, 0, 0, 0, 99]
   end
 end
