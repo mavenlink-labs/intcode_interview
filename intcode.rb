ad0455102b84223d5328847cdbd006037cc18bb2
Chris Bacon <cbacon@mavenlink.com>
2020-08-04 22:36:18 -0600

    add support for param modes

--- intcode.rb
+++ intcode.rb
@@ -12,15 +12,16 @@ class Intcode
     loop do
       return @memory if @memory[@pointer] == 99
 
-      case @memory[@pointer]
+      instruction = translate_instruction
+      case instruction[:opcode]
       when 1
-        add
+        add instruction
       when 2
-        multiply
+        multiply instruction
       when 3
-        use_input
+        use_input instruction
       when 4
-        append_output
+        append_output instruction
       else
         raise ArgumentError, 'unexpected opcode'
       end
@@ -29,37 +30,72 @@ class Intcode
 
   private
 
-  def add
-    operate { |a, b| a + b }
+  def translate_instruction
+    instruction_digits = @memory[@pointer].to_s.rjust(5, '0')
+
+    {
+      param_1_mode: instruction_digits[0].to_i,
+      param_2_mode: instruction_digits[1].to_i,
+      param_3_mode: instruction_digits[2].to_i,
+      opcode: instruction_digits[3..4].to_i
+    }
   end
 
-  def multiply
-    operate { |a, b| a * b }
+  def add(instruction)
+    operate(instruction) { |a, b| a + b }
   end
 
-  def operate
-    read_a_index = @memory[@pointer + 1]
-    read_b_index = @memory[@pointer + 2]
-    save_at_index = @memory[@pointer + 3]
-
-    a = @memory[read_a_index]
-    b = @memory[read_b_index]
-    @memory[save_at_index] = yield a, b
-    @pointer += 4
+  def multiply(instruction)
+    operate(instruction) { |a, b| a * b }
   end
 
-  def use_input
+  def operate(instruction)
+    a = get_param(instruction[:param_1_mode])
+    b = get_param(instruction[:param_2_mode])
+
+    set_param(instruction[:param_3_mode], yield(a, b))
+    advance_pointer
+  end
+
+  def get_param(mode)
+    advance_pointer
+    case mode
+    when 0
+      index = @memory[@pointer]
+      @memory[index]
+    # when 1
+    #   @memory[@pointer]
+    else
+      raise ArgumentError, 'unknown parameter mode'
+    end
+  end
+
+  def set_param(mode, new_value)
+    advance_pointer
+    case mode
+    when 0
+      index = @memory[@pointer]
+      @memory[index] = new_value
+    # when 1
+    #   @memory[@pointer] = new_value
+    else
+      raise ArgumentError, 'unknown parameter mode'
+    end
+  end
+
+  def use_input(instruction)
     next_input = @input.shift
-    input_position = @memory[@pointer + 1]
-
-    @memory[input_position] = next_input
-    @pointer += 2
+    set_param(instruction[:param_1_mode], next_input)
+    advance_pointer
   end
 
-  def append_output
-    output_position = @memory[@pointer + 1]
-    output_value = @memory[output_position]
+  def append_output(instruction)
+    output_value = get_param(instruction[:param_1_mode])
     @output.push(output_value)
-    @pointer += 2
+    advance_pointer
+  end
+
+  def advance_pointer
+    @pointer += 1
   end
 end
