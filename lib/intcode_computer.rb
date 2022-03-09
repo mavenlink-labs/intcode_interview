234b6987370d696f5f4b65c822942b5e6464cffa
Jared Nielson <jnielson@mavenlink.com>
2022-03-09 15:52:49 -0700

    mob next [ci-skip] [ci skip] [skip ci]

--- lib/intcode_computer.rb
+++ lib/intcode_computer.rb
@@ -1,3 +1,10 @@
 class IntcodeComputer
+  class UnsupportedOpCode < StandardError; end
 
+  SUPPORTED_OPCODES = [99]
+
+  def self.operate(instructions)
+    raise UnsupportedOpCode, 'Unexpected item in the bagging area' unless SUPPORTED_OPCODES.include?(instructions.first)
+    instructions
+  end
 end
--- spec/intcode_computer_spec.rb
+++ spec/intcode_computer_spec.rb
@@ -7,6 +7,13 @@ describe IntcodeComputer do
     expect(subject).to_not be_nil
   end
 
+  it 'crashes on unexpected opcodes' do
+    instructions = [101]
+    expect {
+      IntcodeComputer.operate(instructions)
+    }.to raise_error IntcodeComputer::UnsupportedOpCode
+  end
+
   it 'can handle opcode 99' do
     instructions = [99]
 
