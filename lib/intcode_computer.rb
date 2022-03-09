1d00c00e59179fbfa954dc81d82420f3b70ee905
Dario Mercado <dmercado@mavenlink.com>
2022-03-09 15:09:36 -0800

    mob next [ci-skip] [ci skip] [skip ci]

--- lib/intcode_computer.rb
+++ lib/intcode_computer.rb
@@ -8,7 +8,11 @@ class IntcodeComputer
 
     opcode = instructions.first
     if opcode == 1
-      return [1,2,0,1]
+      pointer_1 = instructions[1]
+      pointer_2 = instructions[2]
+      index_pointer = instructions[3]
+      result = instructions[pointer_1] + instructions[pointer_2]
+      instructions[index_pointer] = result
     end
 
     instructions
