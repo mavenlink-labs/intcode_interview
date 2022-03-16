5e06cd8f0556e7fee5eb6897694fbb5ba321b705
Jared Nielson <jnielson@mavenlink.com>
2022-03-16 16:06:43 -0600

    mob next [ci-skip] [ci skip] [skip ci]

--- lib/intcode_computer.rb
+++ lib/intcode_computer.rb
@@ -10,23 +10,42 @@ class IntcodeComputer
     while (opcode = instructions[pointer]) != 99
       raise UnsupportedOpCode, 'Unexpected item in the bagging area' unless SUPPORTED_OPCODES.include?(opcode)
 
-      pointer_1 = instructions[1 + pointer]
-      pointer_2 = instructions[2 + pointer]
-      index_pointer = instructions[3 + pointer]
+      instruction = Instruction.new(pointer: pointer, instructions: instructions)
 
       if opcode == 1
-        result = instructions[pointer_1] + instructions[pointer_2]
+        result = instruction.l_val + instruction.r_val
       end
 
       if opcode == 2
-        result = instructions[pointer_1] * instructions[pointer_2]
+        result = instruction.l_val * instruction.r_val
       end
 
-      instructions[index_pointer] = result
+      instruction.write(result)
 
       pointer += INSTRUCTION_WIDTH
     end
 
     instructions
   end
+end
+
+class Instruction
+  attr_reader :l_pointer, :r_pointer, :l_val, :r_val
+
+  def initialize(pointer:, instructions:)
+    @l_pointer = instructions[1 + pointer]
+    @r_pointer = instructions[2 + pointer]
+    @l_val = instructions[@l_pointer]
+    @r_val = instructions[@r_pointer]
+    @write_pointer = instructions[pointer + 3]
+    @instructions = instructions
+  end
+
+  def write(val)
+    instructions[write_pointer] = val
+  end
+
+  private
+
+  attr_reader :instructions, :write_pointer
 end
